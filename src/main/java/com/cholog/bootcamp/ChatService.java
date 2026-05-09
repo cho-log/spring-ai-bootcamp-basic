4518ab0433c4da236ae30212286b41fbec0e8b6a
신관규 <soundbar91@gmail.com>
2026-05-09 13:55:33 +0900

    feat: OpenAI API 연동

--- src/main/java/com/cholog/bootcamp/ChatService.java
+++ src/main/java/com/cholog/bootcamp/ChatService.java
@@ -1,11 +1,31 @@
 package com.cholog.bootcamp;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.metadata.Usage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
 
+    private final ChatClient chatClient;
+
+    public ChatService(ChatModel chatModel) {
+        this.chatClient = ChatClient.builder(chatModel).build();
+    }
+
     public QuestionAskResponse askQuestion(QuestionAskRequest request) {
-        return null;
+        ChatResponse chatResponse = chatClient.prompt(request.question())
+            .call()
+            .chatResponse();
+        Usage usage = chatResponse.getMetadata().getUsage();
+
+        return QuestionAskResponse.from(
+            chatResponse.getResults().get(0).getOutput().getText(),
+            usage.getPromptTokens(),
+            usage.getCompletionTokens(),
+            usage.getTotalTokens()
+        );
     }
 }
--- src/main/java/com/cholog/bootcamp/QuestionAskResponse.java
+++ src/main/java/com/cholog/bootcamp/QuestionAskResponse.java
@@ -9,6 +9,19 @@ public record QuestionAskResponse(
         Integer completionTokens,
         Integer totalTokens
     ) {
+        public static InnerTokenUsageResponse from(
+            Integer promptTokens, Integer completionTokens, Integer totalTokens
+        ) {
+            return new InnerTokenUsageResponse(promptTokens, completionTokens, totalTokens);
+        }
+    }
 
+    public static QuestionAskResponse from(
+        String answer, Integer promptTokens, Integer completionTokens, Integer totalTokens
+    ) {
+        return new QuestionAskResponse(
+            answer,
+            InnerTokenUsageResponse.from(promptTokens, completionTokens, totalTokens)
+        );
     }
 }
