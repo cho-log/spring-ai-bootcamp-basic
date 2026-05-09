62b7aad6aafbd3901c42a6f5527ea100d56bb140
kjyyjk <rlawnsdud920@gmail.com>
2026-05-09 19:03:55 +0900

    docs: resources/data에 layer_n 복사
    - MarkdownReader에서 classpath로 쉽게 읽어오기 위해

---
+++ src/main/java/com/cholog/bootcamp/MarkdownReader.java
@@ -0,0 +1,37 @@
+package com.cholog.bootcamp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+class MarkdownReader {
+
+    private final Resource[] resources;
+
+    public MarkdownReader(@Value("classpath:data/**/*.md") Resource[] resources) {
+        this.resources = resources;
+    }
+
+    public List<Document> loadAll() {
+        List<Document> allDocuments = new ArrayList<>();
+        for (Resource resource : resources) {
+            MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+                .withHorizontalRuleCreateDocument(true)
+                .withIncludeCodeBlock(false)
+                .withIncludeBlockquote(false)
+                .withAdditionalMetadata("filename", resource.getFilename())
+                .build();
+
+            MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, config);
+            allDocuments.addAll(reader.get());
+        }
+        return allDocuments;
+    }
+}
