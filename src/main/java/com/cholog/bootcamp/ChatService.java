71f066d9983e96cb503ef0f9413e7e7c18156519
신관규 <soundbar91@gmail.com>
2026-05-11 14:03:47 +0900

    feat: FAQ 컨텍스트 제공 추가

--- src/main/java/com/cholog/bootcamp/ChatService.java
+++ src/main/java/com/cholog/bootcamp/ChatService.java
@@ -1,5 +1,10 @@
 package com.cholog.bootcamp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.metadata.Usage;
 import org.springframework.ai.chat.model.ChatModel;
@@ -16,7 +21,17 @@ public class ChatService {
     }
 
     public QuestionAskResponse askQuestion(QuestionAskRequest request) {
-        ChatResponse chatResponse = chatClient.prompt(request.question())
+        String faq = getFAQ();
+
+        ChatResponse chatResponse = chatClient.prompt()
+            .system("""
+                당신은 초록 코퍼레이션에서 고객지원 챗봇을 담당하는 역할입니다.
+                제공된 문서를 참고하여 고객에게 답변을 해주세요.
+                모든 응답은 한국어로 해야하며, 초록 코퍼레이션과 무관한 내용은 다루지 마세요.
+                
+                %s
+                """.formatted(faq))
+            .user(request.question())
             .call()
             .chatResponse();
         Usage usage = chatResponse.getMetadata().getUsage();
@@ -27,5 +42,26 @@ public class ChatService {
             usage.getCompletionTokens(),
             usage.getTotalTokens()
         );
+    }
+
+    private String getFAQ() {
+        StringBuilder sb = new StringBuilder();
+
+        File file = new File("data/layer1_faq");
+        File[] files = file.listFiles();
+
+        for (File fs : files) {
+            sb.append(file.getName()).append('\n');
+            try (BufferedReader br = new BufferedReader(new FileReader(fs))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append('\n');
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 }
