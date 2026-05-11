aea6e109ae5cf0ec7eb858ef96dce5a515ce7140
kjyyjk <rlawnsdud920@gmail.com>
2026-05-11 22:43:58 +0900

    feat: 디버깅용 엔드포인트 추가
    - 참고한 k개 문서 정보 로그로 확인 가능

--- src/main/java/com/cholog/bootcamp/ChatbotController.java
+++ src/main/java/com/cholog/bootcamp/ChatbotController.java
@@ -3,6 +3,7 @@ package com.cholog.bootcamp;
 import java.util.Map;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SearchRequest;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RequestMapping("/api/chat")
 @RestController
 public class ChatbotController {
@@ -22,11 +26,12 @@ public class ChatbotController {
     public ChatbotController(ChatClient.Builder builder, EmbeddingModel embeddingModel, MarkdownReader markdownReader) {
         VectorStore vectorStore = SimpleVectorStore.builder(embeddingModel).build();
         vectorStore.add(markdownReader.loadAll());
-        this.chatClient = builder.defaultAdvisors(
-            QuestionAnswerAdvisor.builder(vectorStore)
-                .searchRequest(SearchRequest.builder().topK(8).build())  // default topK : 4
-                .build()
-        ).build();
+        QuestionAnswerAdvisor questionAnswerAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
+            .searchRequest(SearchRequest.builder().topK(8).build())
+            .build();
+        this.chatClient = builder
+            .defaultAdvisors(questionAnswerAdvisor, new SimpleLoggerAdvisor())
+            .build();
     }
 
     @PostMapping
@@ -36,5 +41,17 @@ public class ChatbotController {
             .call()
             .content();
         return Map.of("answer", answer);
+    }
+
+    @PostMapping("/debug")
+    public Map<String, String> debugChat(@RequestBody ChatRequest request) {
+        Object RETRIEVED_DOCUMENTS = chatClient.prompt()
+            .user(request.question())
+            .call()
+            .chatClientResponse()
+            .context()
+            .get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS);
+        log.info("RETRIEVED_DOCUMENTS: {}", RETRIEVED_DOCUMENTS);
+        return Map.of("answer", "answer");
     }
 }
