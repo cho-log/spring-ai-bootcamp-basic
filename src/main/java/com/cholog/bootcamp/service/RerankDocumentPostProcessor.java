9f1ec402a9991a86f75af29c9279c2ab7ba44710
kjyyjk <rlawnsdud920@gmail.com>
2026-05-19 08:59:55 +0900

    refactor: 리랭크 수정 및 디버깅

--- src/main/java/com/cholog/bootcamp/service/RerankDocumentPostProcessor.java
+++ src/main/java/com/cholog/bootcamp/service/RerankDocumentPostProcessor.java
@@ -23,23 +23,42 @@ public class RerankDocumentPostProcessor implements DocumentPostProcessor {
 
     @Override
     public List<Document> process(Query query, List<Document> documents) {
+        System.out.println("========");
+        for (Document document : documents) {
+            System.out.println(document.getMetadata().get("filename") + " " + document.getText());
+        }
+        System.out.println("========");
         String documentsText = documents.stream()
-            .map(document -> "[[%s]] %s".formatted(document.getId(), document.getText()))
+            .map(document -> """
+                    [[%s]]
+                    source: %s
+                    title: %s
+                    text:
+                    %s
+                    """
+                .formatted(
+                document.getId(),
+                document.getMetadata().get("filename"),
+                document.getMetadata().get("title"),
+                document.getText()
+            ))
             .collect(Collectors.joining("\n\n"));
 
         String result = rerankClient.prompt("""
             # 개요
             당신은 문서들을 재정렬하는 rerank 전문가입니다.
             주어진 질문을 확인하고, 각 문서의 내용이 질문에 답하기 위해 얼마나 필요한 핵심 정보인지를 판단하세요.
-            그리고 우선순위를 기준으로 내림차순 정렬하세요.
+            그리고 우선순위를 기준으로 내림차순 정렬하세요.(우선순위가 높은게 앞에 오게)
             
-            문서 간에 내용이 상반될 경우, 더 구체적이고 세부적인 정보를 담은 문서를 우선순위로 판단하세요.
-            관련성이 동일한 경우 원래 순서를 유지하세요.
+            # **주의사항**
+            - 문서 간 내용이 상반되거나 충돌할 경우, 더 구체적이고 특수한 조건(예외, 제한사항 등)을 다루는 문서를 일반적인 내용보다 반드시 우선시하세요.
+            일반 규칙보다 예외 규칙이 항상 높은 우선순위를 가집니다.
+            - 문서 id를 임의로 누락하지 마세요. 당신은 재정렬만 수행합니다.
             
             # 질문
             %s
             
-            # 문서들 (형식 : [[문서 id]] 문서내용)
+            # 문서들 [[문서 id]]
             %s
             
             # 출력 형식
