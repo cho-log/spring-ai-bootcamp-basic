f287a6d6fded9dc12aa1561d0ef365d444930fbe
kjyyjk <rlawnsdud920@gmail.com>
2026-05-19 10:38:37 +0900

    refactor: 검색되 청크에 해당하는 문서 전체를 컨텍스트에 포함

--- src/main/java/com/cholog/bootcamp/service/ChatbotService.java
+++ src/main/java/com/cholog/bootcamp/service/ChatbotService.java
@@ -1,5 +1,6 @@
 package com.cholog.bootcamp.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +17,11 @@ import org.springframework.ai.document.Document;
 import org.springframework.ai.rag.Query;
 import org.springframework.ai.rag.postretrieval.document.DocumentPostProcessor;
 import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
 import com.cholog.bootcamp.dto.ChatbotRequest;
@@ -34,6 +38,7 @@ public class ChatbotService {
     private final VectorStore vectorStore;
     private final ChatClient.Builder chatClientBuilder;
     private final DocumentPostProcessor documentPostProcessor;
+    private final ResourcePatternResolver resolver;
 
     public ChatbotService(
         VectorStore vectorStore,
@@ -49,28 +54,35 @@ public class ChatbotService {
         this.documentPostProcessor = documentPostProcessor;
         this.vectorStore = vectorStore;
         vectorStore.add(markdownReader.loadAll());
+        this.resolver = new PathMatchingResourcePatternResolver();
     }
 
     public ChatbotResponse chat(String conversationId, ChatbotRequest request) {
-        // List<Document> documents = getDocumentsWithQueryExpansion(request.question());
-        SearchRequest searchRequest = getSearchRequest(request.question(), 10);
+        SearchRequest searchRequest = getSearchRequest(request.question(), 4);
         List<Document> documents = vectorStore.similaritySearch(searchRequest);
-        documents = documentPostProcessor.apply(Query.builder().text(request.question()).build(), documents);
-        List<Document> subList = documents.subList(0, Math.min(documents.size(), 4));
-        String context = getContext(subList);
-        System.out.println("==========");
-        for (Document document : subList) {
-            System.out.println(document.getMetadata().get("filename"));
-            System.out.println(document.getText());
-            System.out.println();
-        }
-        System.out.println("==========");
+        documents = documents.stream()
+            .map(document -> document.getMetadata().get("filename").toString())
+            .distinct()
+            .map(filename -> {
+                try {
+                    return resolver.getResources("classpath:data/**/" + filename)[0];
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            })
+            .map(TextReader::new)
+            .flatMap(reader -> reader.get().stream())
+            .toList();
+
+        String context = getContext(documents);
+        System.out.println(context);
         ChatResponse chatResponse = chatClient.prompt()
             .system("""
                 당신은 초록 고객센터의 챗봇입니다.
                 주어진 [컨텍스트]를 기반으로 [사용자 질문]에 답변해주세요.
                 
                 답변 규칙
+                - 제공된 컨텍스트를 기반으로만 답변하세요. 절대 일반 상식으로 추론하지 마세요.
                 - 만약 주어진 컨텍스트로 답변할 수 없다면 모르겠다고 안내하세요.
                 - 내용이 충돌하는 경우 다음 우선순위를 따라 답변 합니다.
                     - 질문 도메인과 가장 근접한 내용
@@ -100,6 +112,24 @@ public class ChatbotService {
         return context;
     }
 
+    private SearchRequest getSearchRequest(String query, int k) {
+        return SearchRequest.builder()
+            .query(query)
+            .topK(k)
+            .build();
+    }
+
+    public String createConversationId() {
+        return UUID.randomUUID().toString();
+    }
+
+    public void clearConversation(String conversationId) {
+        if (conversationId == null) {
+            throw new NullPointerException("conversationId는 null일 수 없습니다.");
+        }
+        chatMemory.clear(conversationId);
+    }
+
     private List<Document> getDocumentsWithQueryExpansion(String question) {
         MultiQueryExpander queryExpander = MultiQueryExpander.builder()
             .chatClientBuilder(chatClientBuilder)
@@ -120,23 +150,5 @@ public class ChatbotService {
                 (d1, d2) -> d1.getScore() >= d2.getScore() ? d1 : d2 // 충돌 시 높은 score 선택
             ));
         return new ArrayList<>(bestScoreByText.values());
-    }
-
-    private SearchRequest getSearchRequest(String query, int k) {
-        return SearchRequest.builder()
-            .query(query)
-            .topK(k)
-            .build();
-    }
-
-    public String createConversationId() {
-        return UUID.randomUUID().toString();
-    }
-
-    public void clearConversation(String conversationId) {
-        if (conversationId == null) {
-            throw new NullPointerException("conversationId는 null일 수 없습니다.");
-        }
-        chatMemory.clear(conversationId);
     }
 }
