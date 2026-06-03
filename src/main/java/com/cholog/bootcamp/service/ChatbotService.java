399c9470a5a38ce571598d2126d10a7265ea8aac
kjyyjk <rlawnsdud920@gmail.com>
2026-06-03 23:26:42 +0900

    feat: Translation Query Transform 적용

--- src/main/java/com/cholog/bootcamp/service/ChatbotService.java
+++ src/main/java/com/cholog/bootcamp/service/ChatbotService.java
@@ -38,13 +38,10 @@ public class ChatbotService {
 
     public ChatbotResponse chat(ChatbotRequest request) {
         // 질문 전처리
-        Query originalQuery = new Query(request.question());
-        Query transformedQuery = queryTransformer.transform(originalQuery);
-        log.info("Original query: {}", request.question());
-        log.info("Transformed query: {}", transformedQuery.text());
+        Query query = getQuery(request);
 
         // 검색
-        SearchRequest searchRequest = getSearchRequest(transformedQuery, 4);
+        SearchRequest searchRequest = getSearchRequest(query, 4);
 
         List<Document> documents = vectorStore.similaritySearch(searchRequest);
 
@@ -56,7 +53,7 @@ public class ChatbotService {
                 당신은 제공된 [컨텍스트]만을 기반으로 [사용자 질문]에 답변하는 신뢰할 수 있는 초록 고객센터의 챗봇입니다.
                 
                 아래의 규칙을 엄격하게 준수하십시오.
-                - 모든 답변은 컨텍스트에 있는 정보를 기반으로 생성하세요. 절대 컨텍스트에 없는 정보를 추론하거나 사실인 것처럼 대답하지 마세요.
+                - 반드시 모든 답변은 컨텍스트에 있는 정보를 기반으로 생성하세요. 절대 컨텍스트에 없는 정보를 추론하거나 사실인 것처럼 대답하지 마세요.
                 - [컨텍스트]에 참고할 정보가 없어 질문에 대한 정확한 답변을 할 수 없는 경우에만 "죄송합니다만, 제공된 문서에서 고객님이 찾으시는 정보를 찾지 못했어요. 더 자세한 확인이 필요하시다면 상담원에게 문의해주세요."라고 답합니다.
                 - 내용이 충돌하는 경우 다음 우선순위를 따라 답변 합니다.
                     - 질문 도메인과 가장 근접한 내용
@@ -76,6 +73,14 @@ public class ChatbotService {
         String answer = chatResponse.getResult().getOutput().getText();
         Usage usage = chatResponse.getMetadata().getUsage();
         return ChatbotResponse.from(answer, usage, getContextList(documents));
+    }
+
+    private Query getQuery(ChatbotRequest request) {
+        Query originalQuery = new Query(request.question());
+        Query transformedQuery = queryTransformer.transform(originalQuery);
+        log.info("Original Query: {}", originalQuery.text());
+        log.info("Transformed Query: {}", transformedQuery.text());
+        return transformedQuery;
     }
 
     private String getContext(List<Document> documents) {
