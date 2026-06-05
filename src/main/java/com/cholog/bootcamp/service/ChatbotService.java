cd447169c6d14499682a576742ddeade5c398428
kjyyjk <rlawnsdud920@gmail.com>
2026-06-06 01:15:24 +0900

    refactor: 미사용 import 제거

--- src/main/java/com/cholog/bootcamp/service/ChatbotService.java
+++ src/main/java/com/cholog/bootcamp/service/ChatbotService.java
@@ -1,14 +1,9 @@
 package com.cholog.bootcamp.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.metadata.Usage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.rag.preretrieval.query.expansion.QueryExpander;
-import org.springframework.ai.rag.preretrieval.query.transformation.QueryTransformer;
 import org.springframework.stereotype.Service;
 
 import com.cholog.bootcamp.dto.ChatbotRequest;
